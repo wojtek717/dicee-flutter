f2ff12407f3a97999cf44eb48a83714df3178e95
wojtek717 <wojciechkonury@gmail.com>
2019-09-18 16:46:30 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(DicePage());
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
       ),
-    ),
-  );
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    );
   }
 }
